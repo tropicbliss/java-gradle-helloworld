b4294ee5891e3cc47a0d093ea2314e35be11eb26
tropicbliss <tropicbliss@protonmail.com>
2021-12-05 23:46:17 +0800

    Fix dependency management

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,15 @@
 package hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
+
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+
+    LocalTime currentTime = new LocalTime();
+    System.out.println(
+        "This automated train is provided for the security and convenience of the Black Mesa Research Facility personnel. The time is "
+            + currentTime + ".");
   }
 }
